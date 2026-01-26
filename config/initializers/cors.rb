bf146b4f0c612b9107343231e725ba6c68a3e06d
Jales <55924744+GabrielHR0@users.noreply.github.com>
2026-01-25 21:46:04 -0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # Para desenvolvimento local
   allow do
-    origins "http://localhost:5173", "http://localhost:3000", "https://gabrielhr0.github.io"  # Vite e React dev servers
+    origins "http://localhost:5173", "http://localhost:3000", "https://gabrielhr0.github.io", "https://apex-e9ovljk6w-gabrielhr0s-projects.vercel.app",  # Vite e React dev servers
     
     resource "*",
       headers: :any,
