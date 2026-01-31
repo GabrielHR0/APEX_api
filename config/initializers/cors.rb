611201e44dc38048325d8d65f2856fea635b5ee5
Jales <55924744+GabrielHR0@users.noreply.github.com>
2026-01-31 10:35:53 -0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       "http://localhost:5173",
       "http://localhost:3000",
       "https://gabrielhr0.github.io",
-      /\.vercel\.app\z/
+      /\.vercel\.app\z/,
+      "https://apex-six-cyan.vercel.app",
+      "https://apex-git-main-gabrielhr0s-projects.vercel.app",
+      "https://apex-iq1n3w23k-gabrielhr0s-projects.vercel.app",
+      "https://apex-gabrielhr0s-projects.vercel.app"
     )
 
     resource "*",
