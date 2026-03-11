a80f32ba24be407a440427a7aa9eaa80414f29e8
Jales <55924744+GabrielHR0@users.noreply.github.com>
2026-03-11 18:04:52 -0300

    Resolvido bug de criação de address

--- app/controllers/api/v1/company_address_controller.rb
+++ app/controllers/api/v1/company_address_controller.rb
@@ -14,6 +14,9 @@ class Api::V1::CompanyAddressController < Api::V1::ApiController
     return update if @company.address.present?
     authorize @company
     address = @company.build_address(address_params)
+    # Garante associação correta
+    address.enderecavel_id = @company.id
+    address.enderecavel_type = "Company"
     if address.save
       render json: address, status: :created
     else
